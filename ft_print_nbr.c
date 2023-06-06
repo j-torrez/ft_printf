bef50db5de5aa381faa91caeae658f109deeff1c
Juan Bosco Torrez Castillo <juanboscotorrez@gmail.com>
2023-06-06 17:34:15 +0200

    adding print_nbr function

--- ft_print_nbr.c
+++ ft_print_nbr.c
@@ -1,34 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_util.c                                          :+:      :+:    :+:   */
+/*   ft_print_nbr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: johnbosco <johnbosco@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/05 17:18:33 by johnbosco         #+#    #+#             */
-/*   Updated: 2023/06/05 18:34:13 by johnbosco        ###   ########.fr       */
+/*   Created: 2023/06/06 17:33:32 by johnbosco         #+#    #+#             */
+/*   Updated: 2023/06/06 17:33:34 by johnbosco        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
-
-char	*ft_strchr(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	while (1)
-	{
-		if (s[i] == (char)c)
-			return ((char *)s + i);
-		if (s[i] == '\0')
-		{
-			if ((char)c == '\0')
-				return ((char *)s + i);
-			else
-				break ;
-		}
-		i++;
-	}
-	return (0);
-}
