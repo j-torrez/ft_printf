889774b8c276fd02c75e5ebdad7471456d9298e8
Juan Bosco Torrez Castillo <juanboscotorrez@gmail.com>
2023-06-13 00:01:03 +0200

    test_

--- ft_print_char.c
+++ ft_print_char.c
@@ -6,12 +6,12 @@
 /*   By: johnbosco <johnbosco@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 13:31:14 by johnbosco         #+#    #+#             */
-/*   Updated: 2023/06/05 17:33:33 by johnbosco        ###   ########.fr       */
+/*   Updated: 2023/06/13 00:00:54 by johnbosco        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* The reason for returning 1 in this case is to indicate 
-the number of characters written successfully.*/
+the number of characters written successfully Dd.*/
 
 #include	"ft_printf.h"
 
